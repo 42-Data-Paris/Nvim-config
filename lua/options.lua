74b377b920cf4df7bc8bf5f779ee1205b0c1d9de
CrowHugin <kuntz.bastien@gmail.com>
2026-09-10 19:36:33 +0200

    Added some basic config

---
+++ lua/options.lua
@@ -0,0 +1,17 @@
+-- Basic Setup
+vim.cmd("set expandtab")
+vim.cmd("set tabstop=2")
+vim.cmd("set softtabstop=2")
+vim.cmd("set shiftwidth=2")
+vim.cmd("set number")
+vim.cmd("set colorcolumn=80")
+vim.cmd("set relativenumber")
+vim.cmd("syntax on")
+
+-- maping jj and jk to return to normal mode
+vim.api.nvim_set_keymap("i", "jj", "<Esc>", { noremap = false })
+vim.api.nvim_set_keymap("i", "jk", "<Esc>", { noremap = false })
+
+-- Remaping Ctrl U and Ctrl to moves with cursor on the middle of the screen
+vim.keymap.set("n","<C-u>","<C-u>zz", {noremap = true, silent = true})
+vim.keymap.set("n","<C-d>","<C-d>zz", {noremap = true, silent = true})
---
+++ lua/plugins/catppuccin.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("catppuccin").setup({
+        flavour = "mocha",
+        integration = {
+          todo_comments = true,
+          treesitter = true,
+          telescope = true,
+        }
+      })
+      vim.cmd.colorscheme("catppuccin")
+    end,
+  },
+}
